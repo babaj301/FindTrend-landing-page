32acdbc8b4e3514a9290815a0dadad87a2bb06c2
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2025-07-03 21:35:39 +0100

    quick fix

--- src/components/LongText.jsx
+++ src/components/LongText.jsx
@@ -6,7 +6,7 @@ const LongText = () => {
     <div className="bg-white h-[70vh] flex flex-row justify-center items-center md:mt-24">
       <h1 className="text-black text-4xl md:text-6xl font-bold mt-2 ml-16 mr-12 md:m-16 md:w-[50%]">
         Findtrend helps you to increase your productivity and reduce your
-        computer's memory load, <span className="text-gray-400">an</span>
+        computer's memory load, 
         <motion.div
           initial={{ opacity: 0, x: 50 }}
           whileInView={{ opacity: 1, x: 0 }}
@@ -14,7 +14,7 @@ const LongText = () => {
           viewport={{ once: true, amount: 0.5 }}
         >
           <span className="text-gray-400">
-            application that can fulfill your daily browsing needs.
+          an application that can fulfill your daily browsing needs.
           </span>
         </motion.div>
       </h1>
